fb8e796675c47af93a9d08e4e328b214610fa520
Lucas Siqueira <lalvessiqueira8@gmail.com>
2019-05-01 16:17:14 -0300

    melhora logica para o envio de email

--- apps/email/logica.py
+++ apps/email/logica.py
@@ -1,16 +1,38 @@
+from decouple import config
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
-def envia_email_bemvindo(data):
-    context = {
-        'destinatario': [data['perfil']['usuario']['email']],
-        'nome': data['nome_fantasia']
-    }
-    plain_text = f"Olá!! Parabens você está cadastrado no nosso melhor portal de aluguel de salas comerciais do Brasil"
-    html_email = render_to_string('email/bem-vindo.html', context=context)
-    assunto = 'Seja Bem vindo'
-    remetente = 'controlapet@gmail.com'
-    mail = send_mail(assunto, plain_text, remetente, context['destinatario'], html_message=html_email,
-                     fail_silently=False)
-    return mail
+class EnviaEmail:
+    def __init__(self):
+        self.remetente = config('EMAIL_USER')
+
+    def boas_vindas_locador(self, data):
+        assunto = f"Bem vindo ao Locapy, {data['nome_fantasia']}!"
+        destinatario = [data['perfil']['usuario']['email'], ]
+        plain_text = f"Olá!! Parabens você está cadastrado no nosso melhor portal de aluguel de salas comerciais do Brasil"
+        html_email = render_to_string('email/bem-vindo-locador.html', context=data)
+        mail = send_mail(assunto, plain_text, self.remetente, destinatario, html_message=html_email,
+                         fail_silently=False)
+        return mail
+
+    def boas_vindas_locatario(self, data):
+        assunto = f"Bem vindo ao Locapy, {data['nome']}!"
+        destinatario = [data['perfil']['usuario']['email'], ]
+        plain_text = f"Olá!! Parabens você está cadastrado no nosso melhor portal de aluguel de salas comerciais do Brasil"
+        html_email = render_to_string('email/bem-vindo-locatario.html', context=data)
+        mail = send_mail(assunto, plain_text, self.remetente, destinatario, html_message=html_email,
+                         fail_silently=False)
+        return mail
+
+    # def sala_cadastrada(self, data):
+    #     destinatario = [data['perfil']['usuario']['email'], ]
+    #     plain_text = f"Olá!! Parabens você está cadastrado no nosso melhor portal de aluguel de salas comerciais do Brasil"
+    #     html_email = render_to_string('email/bem-vindo-locatario.html', context=data)
+    #     assunto = 'Bem vindo ao Locapy!'
+    #     mail = send_mail(assunto, plain_text, self.remetente, destinatario, html_message=html_email,
+    #                      fail_silently=False)
+    #     return mail
+
+
+envia_email = EnviaEmail
