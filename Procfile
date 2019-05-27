1f74d0307b283255ba7b0e08386a86761f86baa3
Lucas Siqueira <lalvessiqueira8@gmail.com>
2019-05-27 19:51:57 -0300

    adiciona configuracoes para subir no heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn locapy.wsgi --log-file -
+worker: python manage.py celery worker -B -l info
--- locapy/settings/prod.py
+++ locapy/settings/prod.py
@@ -9,3 +9,6 @@ RAVEN_CONFIG = {
     'dsn': config('SENTRY_DSN'),
     'environment': 'prod',
 }
+
+# Como estamos usando a conta gratuita do Heroku, só podemos utilizar 3 conexões no maximo
+BROKER_POOL_LIMIT = 3
